c0988120d35b65443dc85781da73982adbf8931f
Vladimir Bratsiun <v.braciun@insitech.dev>
2025-04-27 23:24:36 +0300

    added unit test for classes

---
+++ src/test/java/com/example/LionManeTest.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mock;
+
+@RunWith(Parameterized.class)
+public class LionManeTest {
+    @Mock
+    private Feline feline;
+
+    private String sex;
+    private boolean expectedHasMane;
+
+    public LionManeTest(String sex, boolean expectedHasMane) {
+        this.sex = sex;
+        this.expectedHasMane = expectedHasMane;
+    }
+
+    @Parameterized.Parameters
+    public static Object[] getManeData() {
+        return new Object[][]{
+                {"Самец", true},
+                {"Самка", false}
+        };
+    }
+
+    @Test
+    public void doesHaveMane() throws Exception {
+        Lion lion = new Lion(feline, sex);
+        Assert.assertEquals(expectedHasMane, lion.doesHaveMane());
+    }
+}
